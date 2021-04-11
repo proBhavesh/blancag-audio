06d7df76a93c0c3cc6d267d05e7329538c8fc6f5
Anam0927 <anam.devdes27@gmail.com>
2021-04-11 14:25:22 +0530

    body padding correction for mobile

--- src/containers/DemosContainer/index.js
+++ src/containers/DemosContainer/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Player from '@vimeo/player';
 
