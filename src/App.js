c224c189396b0e9859f0baa9635be703eb55121b
Anaam <anam.devdes27@gmail.com>
2021-04-26 02:37:03 +0530

    FXes done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import styled from 'styled-components';
 import { isMobile } from 'react-device-detect';
 
 import FxContextComponent from './context/FxContext';
@@ -13,37 +12,20 @@ import { HomePage, DemosPage, MusicPage } from './containers/exporter';
 
 import useDocDims from './helpers/useDocDims';
 
-const WhiteBG = styled.div`
-  position: absolute;
-  width: 100%;
-  height: calc(var(--vh, 1vh) * 100);
-  top: 0;
-  left: 0;
-  background-color: #f5f5f5;
-  z-index: 9000;
-  visibility: ${(props) => (props.showWhite ? 'visible' : 'hidden')};
-`;
-
 const App = () => {
-  const [showWhite, setShowWhite] = useState(true);
-
   const location = useLocation();
   const width = useDocDims();
 
   useEffect(() => {
-    const timer = setTimeout(() => setShowWhite(false), 3000);
-
     function emptyFunc() {}
     isMobile && document.addEventListener('touchstart', emptyFunc, true);
     return () => {
       isMobile && document.addEventListener('touchmove', emptyFunc, true);
-      clearTimeout(timer);
     };
   }, []);
 
   return (
     <>
-      <WhiteBG showWhite={showWhite} />
       {width && (
         <FxContextComponent>
           <LoadingIndicatorContextComponent>
